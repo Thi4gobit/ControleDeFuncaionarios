475b76051dbf4d9d2a098a3c222074f04a44c25e
Thi4gobit <thbittencourt@hotmail.com>
2025-07-15 20:04:11 -0300

    Adicionar arquivos de projeto.

---
+++ ControleDeFuncaionarios/Entidades/Empresa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeFuncaionarios.Entidades
+{
+    /// <summary>
+    /// Class representing a company in the system.
+    /// </summary>
+    public class Empresa
+    {
+        #region Properties
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string RazaoSocial { get; set; } = string.Empty;
+        public string Cnpj { get; set; } = string.Empty;
+
+        #endregion
+
+        #region Relacionamentos
+
+        public List<Funcionario> Funcionarios { get; set; } = new();
+
+        #endregion
+
+    }
+}
---
+++ ControleDeFuncaionarios/Entidades/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeFuncaionarios.Entidades
+{
+    /// <summary>
+    /// Class representing an employee in the system.
+    /// </summary>
+    public class Funcionario
+    {
+        #region Properties
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Nome { get; set; } = string.Empty;
+        public string Cpf { get; set; } = string.Empty;
+        public string Matricula { get; set; } = string.Empty;
+        public DateOnly DataAdmissao { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        public Guid EmpresaId { get; set; } = Guid.Empty;
+
+        #endregion
+
+        #region Relacionamentos
+
+        public Empresa Empresa { get; set; } = new();
+
+        #endregion
+
+    }
+}
